888e31a9deaaefba498c69c2547d223ae2466767
cursomg160 <cursomg160@mtin.es>
2018-05-08 12:48:19 +0200

    greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,8 +14,10 @@ public class Greeter {
 
   /**
    * Para evitar el warning, ponemos un comentario. Muestra Hola.
-   */ 
-  final public String greet(final String someone) {
+   * @param: someone: Nombre de la persona
+   * @return: Devuelve el saludo
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
